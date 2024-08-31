b5a163235ab6d8e5e0994b5946b8babb4e3915f0
Hernan <hershins7@gmail.com>
2024-08-31 16:36:05 -0300

    Creado metodo control MenuP

--- Clases/LogicHelp.cs
+++ Clases/LogicHelp.cs
@@ -13,5 +13,18 @@ namespace EspacioLogicHelp
                 return false;
             }
         }
+
+        public static bool ControlMenuP(int opcion) //control entrada correcta
+        {
+            if (opcion >=1 && opcion <=5 )
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Entrada Incorrecta. Vuelva a Ingresar: ");
+                return false;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,18 +10,35 @@ if (LogicHelp.Existe(ArchivoCadetes) && LogicHelp.Existe(ArchivoCadeteria))
     List<Cadete> ListaCadetes = Cadete.CargarCSVCadetes(ArchivoCadetes);
     Cadeteria cadeteria = Cadeteria.CargarCSVCadeteria(ArchivoCadeteria, ListaCadetes);
 
-    GUI.Menu();
-    Console.Read();
+    string entrada;
+    int opcion;
+    do
+    {
+        do
+        {
+            GUI.Menu();
+            entrada = Console.ReadLine();
+
+        } while (!int.TryParse(entrada, out opcion) || !LogicHelp.ControlMenuP(opcion));
 
 
+        switch (opcion)
+        {
+            case 1:
+            break;
+            case 2:
+            break;
+            case 3:
+            break;
+            case 4:
+            break;
+            case 5:
+            break;
+            default:
+            break;
+        }
 
-
-
-
-
-
-
-
+    } while (opcion >= 1 && opcion <=5);
 }
 else
 {
